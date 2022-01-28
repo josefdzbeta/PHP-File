6d563d8088037ec83c1b34ff863bbcf00e837725
josefdzbeta <jafb19@outlook.com>
2022-01-28 17:04:03 +0100

    css

--- JoseAngelFernandezFicheros/v3/index.php
+++ JoseAngelFernandezFicheros/v3/index.php
@@ -5,14 +5,17 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/style.css">
         <title>Subida de Archivos al servidor</title>
     </head>
     <body>
-        <!--Declaramos archivo que procesa los ficheros y multipart/form-data para habilitar la subida de archivos desde el formulario-->
-        <form action="upload.php" method="post" enctype="multipart/form-data">
-            Selecciona Archivo a subir 
-            <input type="file" name="archivoSubir" id="archivoSubir">
-            <input type="submit" value="Subir Imagen" name="submit">
-        </form>
+        <h1>Subir archivos</h1>
+        <div id="formulario">
+            <!--Declaramos archivo que procesa los ficheros y multipart/form-data para habilitar la subida de archivos desde el formulario-->
+            <form action="upload.php" method="post" enctype="multipart/form-data" class="input-group mb-3">
+                <input type="file" name="archivoSubir" id="archivoSubir" class="form-control">
+                <input type="submit" value="Subir" name="submit">
+            </form>
+        </div>
     </body>
 </html>
